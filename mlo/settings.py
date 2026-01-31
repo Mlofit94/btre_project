11d44a3d8a6e30e7fadf60f1065befa27f00f1cb
Mlofit94 <mlotfit94@yahoo.com>
2026-01-31 14:53:59 -0500

    Added local settings import

--- mlo/settings.py
+++ mlo/settings.py
@@ -153,3 +153,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'lopez12345.ml@gmail.com'
 EMAIL_HOST_PASSWORD = 'jyhi qdpz tdpx qxma' 
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
